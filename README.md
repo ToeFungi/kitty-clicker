9405e7d3f3cace0d8f39c806395c94f529c50b0d
Alex <pickeringacw@gmail.com>
2017-12-27 19:09:35 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Kitty Clicker
+###
+
+### How to play
+To play simply click the kitty. When you have a certain number of kitty points, you'll be able to buy power-ups which last the duration of your session. Each power-up costs a unique price and click per second (CPS) rate which will help your kitty expand across not only space, but time itself!
+
+### How to run locally
+Simply clone the game and drag and drop the `index.html` into your web browser of choice
+
+### Disclaimer
+The game isn't quite finished yet and some features *WILL* be buggy!
---
+++ js/app.js
@@ -0,0 +1,67 @@
+let counter, milkAdd, stringAdd, catnipAdd;
+
+$(".cat img").click(addToCounter);
+
+init();
+
+function init() {
+	counter = 0;
+	milkAdd = 0;
+	stringAdd = 0;
+	catnipAdd = 0;
+
+	pulse();
+}
+
+function addToCounter(value) {
+	value && Number.isInteger(value) ? counter += value : counter++;
+	$(".counter").html(counter);
+}
+
+function removeFromCounter(value) {
+	counter = counter - value;
+	$(".counter").html(counter);
+}
+
+function addPower(evt) {
+	let cost = evt.attributes.price.value;
+	let type = evt.attributes.power.value;
+
+	if (counter < cost) return false;
+	removeFromCounter(cost);
+	
+	switch (type) {
+		case 'milk':
+			if (milkAdd === 0) initInterval(milkAdd);	
+			milkAdd += 1;
+			break;
+		case 'string':
+			if (stringAdd === 0) initInterval(stringAdd);
+			stringAdd += 25;
+			break;
+		case 'catnip':
+			if (catnipAdd === 0) initInterval(catnipAdd);
+			catnipAdd += 100;
+			break;
+		default:
+			return false;
+	}
+}
+
+function initInterval(type) {
+	setInterval(() => {
+		addToCounter(this.milkAdd);
+	}, 1000);
+}
+
+function pulse(back) {
+	$(".cat img").animate({
+        width: (back) ? $(".cat img").width() + 20 : $(".cat img").width() - 20            
+    }, 700);
+
+	$('.cat').animate({          
+        'font-size': (back) ? '100px' : '140px',
+    }, 700, function() {
+    	pulse(!back)
+    });
+}
